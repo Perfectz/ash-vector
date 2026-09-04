e740face5a4ae6771b65f4cb0d0bc5db2bb078e3
Perfectz <perfectz@example.com>
2026-09-04 16:15:41 -0400

    Provide standalone font fallbacks for Vercel

--- web/main.tsx
+++ web/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Game from '../app/game/Game';
 import '../app/globals.css';
+import './standalone.css';
 
 createRoot(document.getElementById('root')!).render(<Game />);
